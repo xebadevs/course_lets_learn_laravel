5793a58f8032d6ac6a8a40d3fe3582ae74b5d9de
xebadevs <xebadevs@gmail.com>
2023-01-19 00:23:12 -0300

    preserving form data when submit fails

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -14,7 +14,7 @@
                     <div class="form-group">
                         <label for="username-register" class="text-muted mb-1"><small>Username</small></label>
                         <input name="username" id="username-register" class="form-control" type="text"
-                            placeholder="Pick a username" autocomplete="off" />
+                            placeholder="Pick a username" autocomplete="off" value="{{ old('username') }}" />
                         @error('username')
                             <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
                         @enderror
@@ -23,7 +23,7 @@
                     <div class="form-group">
                         <label for="email-register" class="text-muted mb-1"><small>Email</small></label>
                         <input name="email" id="email-register" class="form-control" type="text"
-                            placeholder="you@example.com" autocomplete="off" />
+                            placeholder="you@example.com" autocomplete="off" value="{{ old('email') }}" />
                         @error('email')
                             <p class="m-0 small alert alert-danger shadow-sm">{{ $message }}</p>
                         @enderror
