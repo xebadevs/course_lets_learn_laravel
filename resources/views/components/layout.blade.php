660df73fa67035c34500c6741a7d47a191fd9c63
xebadevs <xebadevs@gmail.com>
2023-01-21 14:37:36 -0300

    minor layout details

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -78,7 +78,7 @@
     {{ $slot }}
 
     <footer class="border-top text-center small text-muted py-3">
-        <p class="m-0">Copyright &copy; 2022 <a href="/" class="text-muted">OurApp</a>. All rights reserved.
+        <p class="m-0">Copyright &copy; {{ date('Y') }} <a href="/" class="text-muted">OurApp</a>. All rights reserved.
         </p>
     </footer>
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExampleController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -16,8 +15,6 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', [UserController::class, "showCorrectHomepage"]);
-
-Route::get('/about', [ExampleController::class, "aboutPage"]);
 
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, "login"]);
