d49bdb48c98767899ae617cb2a378a2fb46f6078
xebadevs <xebadevs@gmail.com>
2023-01-22 11:46:58 -0300

    create posts

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,13 @@ use App\Http\Controllers\UserController;
 |
 */
 
+// User related routes
 Route::get('/', [UserController::class, "showCorrectHomepage"]);
-
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, "login"]);
 Route::post('/logout', [UserController::class, "logout"]);
+
+// Blog post related routes
+Route::get('/create-post', [PostController::class, "showCreateForm"]);
+Route::get('/post/{post}', [PostController::class, "showSinglePost"]);
+Route::post('/create-post', [PostController::class, "storeNewPost"]);
