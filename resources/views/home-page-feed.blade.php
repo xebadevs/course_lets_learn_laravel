ea2383126a897bc49ee0559cd83fc961cf9ba2e9
xebadevs <xebadevs@gmail.com>
2023-03-18 22:04:28 -0300

    adding some minimal style change

--- resources/views/home-page-feed.blade.php
+++ resources/views/home-page-feed.blade.php
@@ -13,11 +13,14 @@
                 @endforeach
             </div>
 
-            {{ $posts->links() }}
+            <div class="mt-4">
+                {{ $posts->links() }}
+            </div>
         @else
             <div class="text-center">
                 <h2>Hello <strong> {{ auth()->user()->username }}</strong>, your feed is empty.</h2>
-                <p class="lead text-muted">Your feed displays the latest posts from the people you follow. If you don&rsquo;t
+                <p class="lead text-muted">Your feed displays the latest posts from the people you follow. If you
+                    don&rsquo;t
                     have any friends to follow that&rsquo;s okay; you can use the &ldquo;Search&rdquo; feature in the top
                     menu bar to find content written by people with similar interests and then follow them.</p>
             </div>
