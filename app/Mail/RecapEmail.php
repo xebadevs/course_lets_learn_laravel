6a406b50de949bfb9379d2bc0d92eaf93f21ae89
xebadevs <xebadevs@gmail.com>
2023-04-15 00:43:58 -0300

    recapEmail

---
+++ app/Mail/RecapEmail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RecapEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $postCount = Post::count();
+        $userCount = User::count();
+
+        return $this->subject('Site Recap')->view('recapemail', [
+            'postCount' => $postCount,
+            'userCount' => $userCount
+        ]);
+    }
+}
---
+++ resources/views/recapemail.blade.php
@@ -0,0 +1,5 @@
+<div style="font-family: sans-serif">
+    <p>Here is a quick recap of the application.</p>
+    <p>PostCount: <strong>{{ $postCount }}</strong></p>
+    <p>UserCount: <strong>{{ $userCount }}</strong></p>
+</div>
